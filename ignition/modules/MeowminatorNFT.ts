e21e4dfb1f349a42f276c0bcf7993a1d1d2ba368
flyinglimao <hi@limaois.me>
2025-04-15 19:31:51 +0900

    fix: assets

--- ignition/modules/MeowminatorNFT.ts
+++ ignition/modules/MeowminatorNFT.ts
@@ -10,9 +10,16 @@ const MeowminatorModule = buildModule("MeowminatorModule", (m) => {
   const baseTokenURI = m.getParameter("baseTokenURI", input_baseTokenURI);
 
   // testnet
+  // const Meowminator = m.contract("MeowminatorNFT", [
+  //   new Date("2025-04-15 08:00:00").getTime() / 1000,
+  //   new Date("2025-05-15 05:00:00").getTime() / 1000,
+  //   baseTokenURI,
+  // ]);
+
+  // mainnet
   const Meowminator = m.contract("MeowminatorNFT", [
-    new Date("2025-04-15 08:00:00").getTime() / 1000,
-    new Date("2025-05-15 05:00:00").getTime() / 1000,
+    new Date("2025-04-16 13:00:00").getTime() / 1000,
+    new Date("2025-05-16 13:00:00").getTime() / 1000,
     baseTokenURI,
   ]);
 
